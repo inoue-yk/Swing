de522dc043d83a0fb971609010f8a2a48f3f55a0
Yuki inoue <yukiinoue@cysol.co.jp>
2018-01-10 11:54:09 +0900

    test1

---
+++ frametest/src/SwingTest1.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+public class SwingTest1 {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("SWIMGTEST");
+		frame.setBounds(100,100,400,300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+
+}
---
+++ frametest/src/SwingTest2.java
@@ -0,0 +1,17 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+public class SwingTest2 {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("SwingTest");
+		frame.setBounds(200,200,400,400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton button = new JButton("TewtSwing");
+		frame.getContentPane().add(button,BorderLayout.EAST);
+		frame.setVisible(true);
+	}
+
+}
